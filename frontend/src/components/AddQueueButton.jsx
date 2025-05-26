9a183185883dbf9388959ca477dcd3a417179bd5
Carol Lim <b032020025@student.utem.edu.com>
2025-05-27 07:55:15 +0800

    Change button background color to #00cfff

--- frontend/src/components/AddQueueButton.jsx
+++ frontend/src/components/AddQueueButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button, Flex } from "antd";
+import { BgColorsOutlined } from "@ant-design/icons";
 
 const AddQueueButton = () => (
   <div className="app-footer">
     <Flex vertical gap="small" style={{ width: "100%" }}>
-      <Button type="primary" block>
+      <Button type="primary" style={{ background: "#00cfff" }} block>
         + Queue
       </Button>
     </Flex>
